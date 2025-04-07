b8225a4669394edeb6ffe91ff4ff040ad58ff6b6
Deive_Ex <feliperobos@gmail.com>
2025-04-07 03:55:42 -0300

    Path Finding 2D & 3D

--- Assets/_Game/_Scripts/PathFinding/FindPath2DJob.cs
+++ Assets/_Game/_Scripts/PathFinding/FindPath2DJob.cs
@@ -3,11 +3,10 @@ using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 
-public struct PathNode3D
+public struct PathNode2D
 {
     public int X;
     public int Y;
-    public int Z;
     public int Index;
 
     public int GCost; //Cost from the start node to this node
@@ -18,23 +17,22 @@ public struct PathNode3D
     public bool IsWalkable;
 
     public void CalculateFCost() => FCost = GCost + HCost;
-    public int3 GetPosition() => new (X, Y, Z);
+    public int2 GetPosition() => new (X, Y);
 }
 
 [BurstCompile]
-public struct FindPath3DJob : IJob
+public struct FindPath2DJob : IJob
 {
     #region Fields
 
     private const int MOVE_COST_STRAIGHT = 10;
     private const int MOVE_COST_DIAGONAL = 14;
-    private const int MOVE_COST_DOUBLE_DIAGONAL = 17;
 
-    public int3 StartPos;
-    public int3 EndPos;
-    public int3 GridSize;
+    public int2 StartPos;
+    public int2 EndPos;
+    public int2 GridSize;
     public bool AllowDiagonal;
-    public NativeList<int3> CalculatedPath;
+    public NativeList<int2> CalculatedPath;
 
     #endregion
 
@@ -49,45 +47,39 @@ public struct FindPath3DJob : IJob
 
     #region Private Methods
 
-    private void FindPath(int3 startPos, int3 endPos, int3 gridSize)
+    private void FindPath(int2 startPos, int2 endPos, int2 gridSize)
     {
-        //TODO something is broken when diagonalsare enabled
-        
         //Test Grid
-        NativeArray<PathNode3D> nodeArray = new(gridSize.x * gridSize.y * gridSize.z, Allocator.Temp);
+        NativeArray<PathNode2D> nodeArray = new(gridSize.x * gridSize.y, Allocator.Temp);
 
         for (var x = 0; x < gridSize.x; x++)
         {
             for (var y = 0; y < gridSize.y; y++)
             {
-                for (int z = 0; z < gridSize.z; z++)
+                var node = new PathNode2D
                 {
-                    int3 pos = new(x, y, z);
-                    
-                    var node = new PathNode3D
-                    {
-                        X = x,
-                        Y = y,
-                        Z = z,
-                        Index = CalculateIndex(pos, gridSize),
-                        GCost = int.MaxValue,
-                        HCost = CalculateHeuristicCost(pos, endPos),
-                        IsWalkable = true, //TODO replace with some condition, like checking if the grid is occupied
-                        CameFromNodeIndex = -1 //-1 means this node is currently invalid. THis will be modified when calculating the path
-                    };
+                    X = x,
+                    Y = y,
+                    Index = CalculateIndex(x, y, gridSize),
+                    GCost = int.MaxValue,
+                    HCost = CalculateHeuristicCost(new int2(x, y), endPos),
+                    IsWalkable = true, //TODO replace with some condition, like checking if the grid is occupied
+                    CameFromNodeIndex = -1 //-1 means this node is currently invalid. THis will be modified when calculating the path
+                };
 
-                    node.CalculateFCost();
-                    nodeArray[node.Index] = node;
-                }
+                node.CalculateFCost();
+                nodeArray[node.Index] = node;
             }
         }
 
-        var startNode = nodeArray[CalculateIndex(startPos, gridSize)];
+        
+
+        var startNode = nodeArray[CalculateIndex(startPos.x, startPos.y, gridSize)];
         startNode.GCost = 0;
         startNode.CalculateFCost();
         nodeArray[startNode.Index] = startNode;
 
-        var endNodeIndex = CalculateIndex(endPos, gridSize);
+        var endNodeIndex = CalculateIndex(endPos.x, endPos.y, gridSize);
 
         NativeList<int> openList = new(Allocator.Temp);
         NativeList<int> closedList = new(Allocator.Temp);
@@ -105,7 +97,7 @@ public struct FindPath3DJob : IJob
         closedList.Dispose();
     }
 
-    private void TraverseGrid(NativeArray<PathNode3D> nodeArray, NativeList<int> openList, NativeList<int> closedList, int endNodeIndex, int3 gridSize)
+    private void TraverseGrid(NativeArray<PathNode2D> nodeArray, NativeList<int> openList, NativeList<int> closedList, int endNodeIndex, int2 gridSize)
     {
         var neighbourOffsetArray = GetNeighboursOffsetArray();
         
@@ -141,17 +133,17 @@ public struct FindPath3DJob : IJob
     private void CheckNeighbour(
         NativeList<int> openList, 
         NativeList<int> closedList, 
-        NativeArray<PathNode3D> nodeArray, 
-        int3 gridSize, 
-        PathNode3D currentNode3D, 
+        NativeArray<PathNode2D> nodeArray, 
+        int2 gridSize, 
+        PathNode2D currentNode2D, 
         int currentNodeIndex, 
-        int3 neighbourPos
+        int2 neighbourPos
         )
     {
         if (!IsPositionInsideGrid(neighbourPos, gridSize))
             return;
 
-        var neighbourIndex = CalculateIndex(neighbourPos, gridSize);
+        var neighbourIndex = CalculateIndex(neighbourPos.x, neighbourPos.y, gridSize);
 
         if (closedList.Contains(neighbourIndex))
             return;
@@ -161,7 +153,7 @@ public struct FindPath3DJob : IJob
         if (!neighbourNode.IsWalkable)
             return;
 
-        var tentativeGCost = currentNode3D.GCost + CalculateHeuristicCost(currentNode3D.GetPosition(), neighbourPos);
+        var tentativeGCost = currentNode2D.GCost + CalculateHeuristicCost(currentNode2D.GetPosition(), neighbourPos);
 
         if (tentativeGCost >= neighbourNode.GCost)
             return;
@@ -175,22 +167,26 @@ public struct FindPath3DJob : IJob
             openList.Add(neighbourNode.Index);
     }
 
-    private int CalculateIndex(int3 pos, int3 gridSize)
+    private int CalculateIndex(int x, int y, int2 gridSize)
     {
-        return (pos.x * gridSize.y * gridSize.z) + (pos.y * gridSize.z) + pos.z;
+        return x + y * gridSize.x;
     }
 
-    private int CalculateHeuristicCost(int3 posA, int3 posB)
+    private int CalculateHeuristicCost(int2 posA, int2 posB)
     {
-        int xDistance = math.abs(posA.x - posB.x);
-        int yDistance = math.abs(posA.y - posB.y);
-        int zDistance = math.abs(posA.z - posB.z);
+        var xDistance = math.abs(posA.x - posB.x);
+        var yDistance = math.abs(posA.y - posB.y);
+        
+        if (AllowDiagonal)
+        {
+            var remaining = math.abs(xDistance - yDistance);
+            return MOVE_COST_DIAGONAL * math.min(xDistance, yDistance) + MOVE_COST_STRAIGHT * remaining;
+        }
 
-        //Manhattan Distance
-        return (xDistance + yDistance + zDistance) * MOVE_COST_STRAIGHT;
+        return (xDistance + yDistance) * MOVE_COST_STRAIGHT;
     }
 
-    private int GetLowestCostFNodeIndex(NativeList<int> openList, NativeArray<PathNode3D> nodeArray)
+    private int GetLowestCostFNodeIndex(NativeList<int> openList, NativeArray<PathNode2D> nodeArray)
     {
         var lowestCostNode = nodeArray[openList[0]];
 
@@ -205,18 +201,16 @@ public struct FindPath3DJob : IJob
         return lowestCostNode.Index;
     }
 
-    private bool IsPositionInsideGrid(int3 gridPos, int3 gridSize)
+    private bool IsPositionInsideGrid(int2 gridPos, int2 gridSize)
     {
         return
             gridPos.x >= 0 &&
             gridPos.y >= 0 &&
-            gridPos.z >= 0 &&
             gridPos.x < gridSize.x &&
-            gridPos.y < gridSize.y &&
-            gridPos.z < gridSize.z;
+            gridPos.y < gridSize.y;
     }
 
-    private void RetracePath(NativeArray<PathNode3D> nodeArray, NativeList<int3> path, int endNodeIndex)
+    private void RetracePath(NativeArray<PathNode2D> nodeArray, NativeList<int2> path, int endNodeIndex)
     {
         CalculatedPath.Clear();
         var endNode = nodeArray[endNodeIndex];
@@ -235,46 +229,22 @@ public struct FindPath3DJob : IJob
         }
     }
 
-    private NativeArray<int3> GetNeighboursOffsetArray()
+    private NativeArray<int2> GetNeighboursOffsetArray()
     {
-        NativeArray<int3> offsets = new(AllowDiagonal ? 26 : 6, Allocator.Temp);
+        NativeArray<int2> offsets = new(AllowDiagonal ? 8 : 4, Allocator.Temp);
         
-        //Cardinal directions
-        offsets[0] = new int3(-1,  0,  0);
-        offsets[1] = new int3(+1,  0,  0);
-        offsets[2] = new int3( 0, -1,  0);
-        offsets[3] = new int3( 0, +1,  0);
-        offsets[4] = new int3( 0,  0, -1);
-        offsets[5] = new int3( 0,  0, +1);
+        offsets[0] = new int2(-1, 0);
+        offsets[1] = new int2(+1, 0);
+        offsets[2] = new int2(0, -1);
+        offsets[3] = new int2(0, +1);
 
-        if (!AllowDiagonal)
-            return offsets;
-        
-        //Diagonals on Y = 0
-        offsets[0] = new int3(-1, 0, +1);
-        offsets[1] = new int3(+1, 0, +1);
-        offsets[0] = new int3(-1, 0, -1);
-        offsets[1] = new int3(+1, 0, -1);
-        
-        //Diagonals on Y = 1
-        offsets[0] = new int3(-1, 1,  0);
-        offsets[1] = new int3(+1, 1,  0);
-        offsets[4] = new int3( 0, 1, -1);
-        offsets[5] = new int3( 0, 1, +1);
-        offsets[0] = new int3(-1, 1, +1);
-        offsets[1] = new int3(+1, 1, +1);
-        offsets[0] = new int3(-1, 1, -1);
-        offsets[1] = new int3(+1, 1, -1);
-        
-        //Diagonals on Y = -1
-        offsets[0] = new int3(-1, -1,  0);
-        offsets[1] = new int3(+1, -1,  0);
-        offsets[4] = new int3( 0, -1, -1);
-        offsets[5] = new int3( 0, -1, +1);
-        offsets[0] = new int3(-1, -1, +1);
-        offsets[1] = new int3(+1, -1, +1);
-        offsets[0] = new int3(-1, -1, -1);
-        offsets[1] = new int3(+1, -1, -1);
+        if (AllowDiagonal)
+        {
+            offsets[4] = new int2(-1, -1);
+            offsets[5] = new int2(-1, +1);
+            offsets[6] = new int2(+1, -1);
+            offsets[7] = new int2(+1, +1);
+        }
 
         return offsets;
     }
